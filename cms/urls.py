846ffe9545c012f32e3315dc14029e33aa480b35
Jayasurya <jsuryakt@gmail.com>
2020-12-06 00:17:35 +0530

    Change TimeZone and Home URL

--- cms/urls.py
+++ cms/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from blog.views import PostListView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogs/', include('blog.urls')),
     path('accounts/', include('account.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('', PostListView.as_view(), name="index")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
