b3bffca8a440580d0bac3a30677a31a4383903b1
Jayasurya <jsuryakt@gmail.com>
2020-12-15 18:05:33 +0530

    HTML Email Template as view

--- account/urls.py
+++ account/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('author-request', AuthorRequestView.as_view(), name="author-request"),
     path('my-posts', MyPostsView.as_view(), name="my-posts"),
  
-    path('password/reset/', PasswordResetView.as_view(template_name="account/password_reset.html", email_template_name="account/password_reset_email.html"), name='password_reset'),
+    path('password/reset/', PasswordResetView.as_view(template_name="account/password_reset.html", html_email_template_name="account/password_reset_email.html"), name='password_reset'),
     path('password/reset/done/', PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"), name='password_reset_done'),
     path('password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html"), name='password_reset_confirm'),
     path('password/reset/complete/', PasswordResetCompleteView.as_view(template_name="account/password_reset_complete.html"), name='password_reset_complete'),
